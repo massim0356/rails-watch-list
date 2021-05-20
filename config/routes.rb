b719137a284e9c39d0afa9c39671edebf9318fde
Massim Shah <massimshah@hotmail.com>
2021-05-20 14:05:20 +0900

    created models and list controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'rest-client'
+Movie.delete_all
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+rest_reponse = RestClient.get(url)
+response = JSON.parse(rest_reponse.body)
+
+response['results'].each do |movie_hash|
+  Movie.create!(title: movie_hash["title"], overview: movie_hash["overview"], poster_url: movie_hash["poster_path"], rating: movie_hash["vote_average"])
+end
