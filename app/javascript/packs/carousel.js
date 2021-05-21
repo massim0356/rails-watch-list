902dfb710148aee1b779375f51f74aa8c3f1b557
Massim Shah <massimshah@hotmail.com>
2021-05-21 16:45:27 +0900

    styling

--- app/javascript/packs/carousel.js
+++ app/javascript/packs/carousel.js
@@ -1,7 +1,7 @@
 const initCarousel = () => {
   const galleryContainer = document.querySelector('.gallery-container');
   const galleryControlsContainer = document.querySelector('.gallery-controls');
-  const galleryControls = ['previous', 'add', 'next'];
+  const galleryControls = ['previous', 'next'];
   const galleryItems = document.querySelectorAll('.gallery-item');
 
   class Carousel {
